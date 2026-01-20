5910cee3667a223ae10fd4726276a5378dedbf54
lucafan8973 <ferexmoto6@gmail.com>
2026-01-21 02:13:27 +0900

    second commit

--- src/components/checkout/CryptoPaymentSourceSelector.jsx
+++ src/components/checkout/CryptoPaymentSourceSelector.jsx
@@ -1,7 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import LoadingScreen from "../common/LoadingScreen";
-import { useState, useEffect } from "react";
 import {
   resetCheckout
 } from "../../redux/checkout/checkoutActions";
@@ -28,7 +27,7 @@ export default function CryptoPaymentSelection() {
   return (
     <div className="h-full px-4 py-12 flex flex-col items-center text-white">
       <div>
-        <ConnectWalletButton className="mt-6 w-full py-2 border border-white text-white hover:bg-white/10 rounded-full" userId="sousa" />
+        {/* <ConnectWalletButton className="mt-6 w-full py-2 border border-white text-white hover:bg-white/10 rounded-full" userId="sousa" /> */}
         <button
           onClick={() => {
             dispatch(resetCheckout());
